062584f3f86b4b0fd256db2032e85cbf8b8f89c4
NickLovera <60933011+NickLovera@users.noreply.github.com>
2020-02-12 21:35:30 -0600

    Update ControllerTransfer.java
    Added Transfer Function. 'recievingAccount' may have the wrong label type or name.

--- ControllerTransfer.java
+++ ControllerTransfer.java
@@ -1,17 +1,46 @@
 package sample;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class ControllerTransfer {
 
-    @FXML
-    public TextField amountTransfer;
-    @FXML
-    public TextArea detailsTransfer;
+	@FXML
+	public TextField amountTransfer;
+	@FXML
+	public TextArea detailsTransfer;
+	@FXML
+	public Label receivingAccount
 
-    public void initialize() {
+	public void initialize() {
 
-    }
+	}
+
+	//Transfering
+	private boolean transfer() {
+
+		Transaction transfer = new Transaction();
+
+		transfer.setType("Transfer");
+		transfer.setAccount(account().getAccountNumber());
+		transfer.setReceivingAccount(recievingAccount());
+		transfer.setAmount(amountTransfer);
+		transfer.setDetails(detailsTransfer);
+		//Getting Date
+		LocalDate localDate = java.time.LocalDate.now();
+		String date = new SimpleDateFormat("dd-MM-yyyy").format(localDate);
+		transfer.setDate(date);
+
+
+		return Datasource.getInstance.saveTransaction(transfer);
+	}
+
+	private Account account() {
+		return Datasource.getInstance.account();
+
+	}
 }
