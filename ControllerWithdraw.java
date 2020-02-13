6a230aea432897cb5b012717a393e928e7b4834a
NickLovera <60933011+NickLovera@users.noreply.github.com>
2020-02-12 21:24:22 -0600

    Update ControllerWithdraw.java
    Added withdrawal function

--- ControllerWithdraw.java
+++ ControllerWithdraw.java
@@ -1,17 +1,41 @@
 package sample;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class ControllerWithdraw {
-    @FXML
-    public TextField amountWithdraw;
-    @FXML
-    public TextArea detailsWithdraw;
+public class Controllerwithdrawal {
 
-    public void initialize() {
+	@FXML
+	public TextField amountwithdrawal;
+	
+	@FXML
+    public TextArea detailswithdrawal;
 
-    }
+	public void initialize() {
+
+	}
+
+	public boolean withdrawal(){
+		Transaction withdrawal = new Transaction();
+		//Setting transaction details
+		withdrawal.setType("withdrawal");
+		withdrawal.setAccount(account().getAccountNumber());
+		withdrawal.setAmount(amountwithdrawal);
+		withdrawal.setDetails(detailswithdrawal);
+		//Getting Date
+		LocalDate localDate = java.time.LocalDate.now();
+		String date = new SimpleDateFormat("dd-MM-yyyy").format(localDate);
+		withdrawal.setDate(date);
+        //Saving Transaction
+		return Datasource.getInstance.saveTransaction(withdrawal);
+	}
+
+
+	private Account account(){
+		return Datasource.getInstance.account();
+	}
 }
-
