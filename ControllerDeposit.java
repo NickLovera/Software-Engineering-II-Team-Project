6f057f303ae3de59666eaf1eea14a0bd01d42851
NickLovera <60933011+NickLovera@users.noreply.github.com>
2020-02-12 21:21:12 -0600

    Update ControllerDeposit.java
    Added deposit function

--- ControllerDeposit.java
+++ ControllerDeposit.java
@@ -1,17 +1,41 @@
 package sample;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class ControllerDeposit {
 
-    @FXML
-    public TextField amountDeposit;
-    @FXML
+	@FXML
+	public TextField amountDeposit;
+	
+	@FXML
     public TextArea detailsDeposit;
 
-    public void initialize() {
+	public void initialize() {
 
-    }
+	}
+
+	public boolean deposit(){
+		Transaction deposit = new Transaction();
+		//Setting transaction details
+		deposit.setType("Deposit");
+		deposit.setAccount(account().getAccountNumber());
+		deposit.setAmount(amountDeposit);
+		deposit.setDetails(detailsDeposit);
+		//Getting Date
+		LocalDate localDate = java.time.LocalDate.now();
+		String date = new SimpleDateFormat("dd-MM-yyyy").format(localDate);
+		deposit.setDate(date);
+
+		return Datasource.getInstance.saveTransaction(deposit);
+	}
+
+
+	private Account account(){
+		return Datasource.getInstance.account();
+	}
 }
